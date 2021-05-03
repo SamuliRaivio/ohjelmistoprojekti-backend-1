95f0dbb9ff8a24095d38539e1a88fb19cc46763d
SamuliRaivio <64201196+SamuliRaivio@users.noreply.github.com>
2021-05-03 15:38:14 +0300

    kommentti

---
+++ src/main/java/com/example/ohjelmistoprojektiBackend/OhjelmistoprojektiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ohjelmistoprojektiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhjelmistoprojektiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhjelmistoprojektiBackendApplication.class, args);
+	}
+
+}
